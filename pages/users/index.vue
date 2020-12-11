13384e500b98cb1d7f808987fc44915eb0df7d4d
yyavo <yann.yavo@i-run.fr>
2020-12-11 08:16:33 +0100

    navigation-lien-programatic

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Hello</h1>
+  <div>
+    <input type="text" v-model="userid">
+    <button @click="onLoadUser">Load Users</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "users.vue"
+  name: "users.vue",
+  data() {
+    return {
+      userid: ''
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userid)
+    }
+  }
 }
 </script>
 
