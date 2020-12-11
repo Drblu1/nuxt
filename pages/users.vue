1dc8cbf00673a5985fa8e074fb6101db0fed940d
yyavo <yann.yavo@i-run.fr>
2020-12-11 08:47:23 +0100

    nested-routes

---
+++ pages/users.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="text" v-model="userid">
+    <button @click="onLoadUser">Load Users</button>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userid: ''
+    }
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userid)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <div>
-    <input type="text" v-model="userid">
-    <button @click="onLoadUser">Load Users</button>
-  </div>
+  <p>Plz enter  an id</p>
 </template>
-
-<script>
-export default {
-  name: "users.vue",
-  data() {
-    return {
-      userid: ''
-    }
-  },
-  methods: {
-    onLoadUser() {
-      this.$router.push('/users/' + this.userid)
-    }
-  }
-}
-</script>
-
-<style scoped>
-
-</style>
